71ef9d0a3ed5c1121d72deb0d608320d8280d961
Chema <josem.perram@educa.jcyl.es>
2023-11-13 18:01:27 +0100

    Creada estructura de clases

---
+++ src/Vehiculo.java
@@ -0,0 +1,65 @@
+public class Vehiculo {
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private String tipoCombustible;
+    private String motor;
+    private String numCv;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getNumCv() {
+        return numCv;
+    }
+
+    public void setNumCv(String numCv) {
+        this.numCv = numCv;
+    }
+}
