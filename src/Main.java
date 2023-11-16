00842ce43478426aaa1c957aac88e44ab5275e58
Chema <josem.perram@educa.jcyl.es>
2023-11-16 16:04:10 +0100

    Se crea el main con una ejecución de pruebas.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Proceso de alta
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setMatricula("01234X");
+        vehiculo.setMarca("Seat");
+        vehiculo.setModelo("127");
+        vehiculo.setColor("Blanco");
+        vehiculo.setMotor("2.0");
+        vehiculo.setNumCv("105");
+        vehiculo.setTipoCombustible("Diesel");
+
+        // Proceso de alta de Cliente
+        Cliente cliente = new Cliente();
+        cliente.setDni("0123456");
+        cliente.setNombre("Cliente1");
+        cliente.setApellidos("Apellidos1 Appellidos2");
+        cliente.setDireccion("Dirección Cliente1");
+
+        // Creamos una venta
+        Venta venta = new Venta();
+        venta.setCodVenta("001");
+        venta.setFechaVenta("16/11/2023");
+        venta.setPrecioVenta("5.000€");
+        venta.setFormaPago("Contado");
+        venta.setCliente(cliente);
+        venta.setVehiculo(vehiculo);
+
+        System.out.println("Venta realizada:" + venta);
     }
 }
