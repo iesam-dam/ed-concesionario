2a1ea3d3cf7702d79ce13b99d7de93ef1a7462af
Chema <josem.perram@educa.jcyl.es>
2024-01-25 14:37:55 +0200

    ACtualización de la clase por nuevos métodos

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // Proceso de alta
-        Vehiculo vehiculo = new Vehiculo();
-        vehiculo.setMatricula("01234X");
-        vehiculo.setMarca("Seat");
-        vehiculo.setModelo("127");
-        vehiculo.setColor("Blanco");
-        vehiculo.setMotor("2.0");
-        vehiculo.setNumCv("105");
-        vehiculo.setTipoCombustible("Diesel");
+        Vehiculo vehiculo1 = new Vehiculo();
+        vehiculo1.setMatricula("01234X");
+        vehiculo1.setMarca("Seat");
+        vehiculo1.setModelo("127");
+        vehiculo1.setColor("Blanco");
+        vehiculo1.setMotor("2.0");
+        vehiculo1.setNumCv("105");
+        vehiculo1.setTipoCombustible("Diesel");
+
+        Vehiculo vehiculo2 = new Vehiculo();
+        vehiculo2.setMatricula("21312");
+        vehiculo2.setMarca("Se123123at");
+        vehiculo2.setModelo("123123");
+        vehiculo2.setColor("123");
+        vehiculo2.setMotor("122.0");
+        vehiculo2.setNumCv("123");
+        vehiculo2.setTipoCombustible("Gasolina");
+
+        List<Vehiculo> vehiculos = new ArrayList<>();
+        vehiculos.add(vehiculo1);
+        vehiculos.add(vehiculo2);
 
         // Proceso de alta de Cliente
         Cliente cliente = new Cliente();
@@ -24,7 +40,10 @@ public class Main {
         venta.setPrecioVenta("5.000€");
         venta.setFormaPago("Contado");
         venta.setCliente(cliente);
-        venta.setVehiculo(vehiculo);
+        venta.setVehiculo(vehiculos);
+
+        Vehiculo veh1 = venta.getVehiculo().get(0);
+        Vehiculo veh2 = venta.getVehiculo().get(1);
 
         System.out.println("Venta realizada:" + venta);
     }
